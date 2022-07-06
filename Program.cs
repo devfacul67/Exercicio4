0e4e28815ea5bdf1dcd0fee8f36df24040b5a6dc
curso.ads <curso.ads@LAB06-07>
2022-07-06 18:26:10 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont;
+            Console.WriteLine("Programa para realizar a contagem de 1 a 50.\n Aperte qualquer tecla para continuar...");
+            Console.ReadKey();
+
+            for (cont = 1; cont <= 50; cont++)
+            {
+                Console.WriteLine($"Número {cont}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
